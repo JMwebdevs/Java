5ad60829b8fa25ca9c7d4f3189e367cff4dc3bd0
JMwebdevs <lapenidjohnmark@gmail.com>
2025-07-28 21:14:34 +0800

    Day - 1 | Java

---
+++ myFirstProgram.java
@@ -0,0 +1,7 @@
+public class myFirstProgram {
+  public static void main(String[] args){
+    System.out.print("Kupal kaba boss?\n");
+    System.out.print("John Mark Lapenid Pogi!");
+    // System.out.println("John Mark Lapenid Pogi!");
+  }
+}
